635893b6394cffaea7d3cef6d6e24921f217a5d2
Nathnael Desalegn <nathnaeldes@gmail.com>
2024-08-05 13:31:11 +0300

    front end uploaded

--- src/components/Reports/ColorsUtil.js
+++
@@ -1,8 +0,0 @@
-export const chartColors = [
-    '--blue-700',   // Light Blue
-    '--blue-900',   // Light Blue
-    '--blue-800',   // Light Blue
-    '--blue-600',   // Medium Blue
-    '--blue-400',   // Base Blue
-    '--blue-300',   // Darker Blue
-];
